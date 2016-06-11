ff8172ccbb3645c52c3be01fe8e8b39f740026d8
Hidetsugu Tamaki <Hidetsugu Tamaki>
2016-06-11 14:07:12 +0900

    Add sample code of Xamarin

---
+++ 04-Xamarin/NativeSample/NativeSample/NativeSample/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NativeSample
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+
+        public static async Task<String> GetSearchResult(String str)
+        {
+            var httpClient = new HttpClient();
+            var url = "https://raw.githubusercontent.com/xamarin/xamarin-" + str + "/master/README.md";
+            return await httpClient.GetStringAsync(url);
+        }
+    }
+}
